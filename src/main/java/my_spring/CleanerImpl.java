5649d0f1e9e2dffd90ee050040509114a1e87d49
PawelMelnik <melnikpavel98@gmail.com>
2021-08-26 12:41:51 +0300

    Added my_spring from evgeniyB

---
+++ src/main/java/my_spring/CleanerImpl.java
@@ -0,0 +1,17 @@
+package my_spring;
+
+/**
+ * @author Evgeny Borisov
+ */
+public class CleanerImpl implements Cleaner {
+
+    @InjectRandomInt(min = 3, max = 5)
+    private int repeat;
+
+    @Override
+    public void clean() {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("VVVVVVVvvvvvvvvvvvvvvvvv");
+        }
+    }
+}
---
+++ src/main/java/my_spring/ObjectFactory.java
@@ -0,0 +1,48 @@
+package my_spring;
+
+import lombok.Getter;
+import lombok.SneakyThrows;
+import org.reflections.Reflections;
+
+import java.util.Set;
+
+/**
+ * @author Evgeny Borisov
+ */
+public class ObjectFactory {
+    @Getter
+    private static ObjectFactory instance = new ObjectFactory();
+    private Config config = new JavaConfig();
+    private Reflections scanner = new Reflections("my_spring");
+
+    @SneakyThrows
+    public <T> T createObject(Class<T> type) {
+        type = resolveImple(type);
+
+        T t = type.getDeclaredConstructor().newInstance();
+
+
+
+
+
+
+        return t;
+    }
+
+    private <T> Class<T> resolveImple(Class<T> type) {
+        if (type.isInterface()) {
+            Class<T> implClass = config.getImplClass(type);
+            if (implClass == null) {
+                Set<Class<? extends T>> classes = scanner.getSubTypesOf(type);
+                if (classes.size() != 1) {
+                    throw new IllegalStateException(type + " has 0 or more than one impl was found, please update your config");
+                }
+                implClass = (Class<T>) classes.iterator().next();
+            }
+            type = implClass;
+        }
+        return type;
+    }
+
+
+}
